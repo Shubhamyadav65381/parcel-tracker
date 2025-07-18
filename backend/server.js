de60b860f0d1589d3ab3f5557ddc3ff7df92b64c
Shubham yadav <shubham65381@gmail.com>
2025-07-18 14:46:17 +0530

    updated in each files

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const userRoutes = require("./routes/userRoutes");
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use('/api/admin', require('./routes/adminRoutes'));
+app.use("/api/users", userRoutes);
+app.use(cors({
+  origin: "https://parcel-tracker-frontend-m67f.onrender.com", 
+  credentials: true,
+}));
 
 const PORT = process.env.PORT || 5000;
 
