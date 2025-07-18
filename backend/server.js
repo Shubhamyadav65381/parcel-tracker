dd86d0bdc44c82d43f099ab1b3b049be545b39b2
Shubhamyadav65381 <shubham65381@gmail.com>
2025-07-18 16:05:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use('/api/admin', require('./routes/adminRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/parcels', require('./routes/parcelRoutes'));
 app.use(cors({
   origin: "https://parcel-tracker-frontend-m67f.onrender.com", 
   credentials: true,
 }));
-
-app.use('/api/admin', require('./routes/adminRoutes'));
-app.use("/api/users", userRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 
@@ -22,9 +21,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection failed:',err));
 
-// Import Routes
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/parcels', require('./routes/parcelRoutes'));
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
