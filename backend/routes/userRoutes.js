7abfd876abfbc17f462c3df4d94def9acc0b793d
Shubham yadav <shubham65381@gmail.com>
2025-07-18 15:39:24 +0530

    updated server.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/userController');
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('api/users/register', registerUser);
+router.post('api/users/login', loginUser);
 
 module.exports = router;
