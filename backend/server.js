e3f7414ec2bd6f2db26a00ef6c42c8b0000a772a
Shubhamyadav65381 <shubham65381@gmail.com>
2025-07-18 15:44:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const cors = require('cors');
-const userRoutes = require("./routes/userRoutes");
 const mongoose = require('mongoose');
+const userRoutes = require("./routes/userRoutes");
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-
-
 app.use(express.json());
-
 app.use(cors({
-  origin: "https://parcel-tracker-1.onrender.com",
+  origin: "https://parcel-tracker-frontend-m67f.onrender.com", 
   credentials: true,
 }));
 
+app.use('/api/admin', require('./routes/adminRoutes'));
+app.use("/api/users", userRoutes);
 
-app.use("/api/users", require('./routes/userRoutes')); 
-app.use("/api/admin", require('./routes/adminRoutes'));
-app.use("/api/parcels", require('./routes/parcelRoutes'));
 
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection failed:', err));
+  .catch(err => console.log('MongoDB connection failed:',err));
+
+// Import Routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/parcels', require('./routes/parcelRoutes'));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
